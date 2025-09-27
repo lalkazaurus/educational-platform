875e0edc606caf43a77c30329ec1cb9eddc5dd5a
lalkazaurus <indiraptor63983@gmail.com>
2025-09-27 14:21:17 +0300

    Login page was added

---
+++ client/src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,48 @@
+import { useForm } from "react-hook-form"
+import type { LoginDto } from "../../types/login.dto"
+import { login } from "../../api/auth.api"
+import { useTranslation } from "react-i18next"
+import styles from "./LoginPage.module.css"
+
+export default function LoginPage() {
+    const { register, reset, formState: {errors}, handleSubmit } = useForm<LoginDto>({
+        mode: "onChange"
+    })
+
+    const { t } = useTranslation()
+
+    async function onSubmit(data: LoginDto) {
+        await login(data)
+    }
+    
+    return <>
+        <form onSubmit={handleSubmit(onSubmit)}>
+
+            <label>{t("email")}</label>
+            <input {...register("email", {
+                required:  'Email is required',
+                pattern: {
+                    value:
+								/[a-z0-9._%+!$&*=^|~#%'`?{}/-]+@([a-z0-9-]+\.){1,}([a-z]{2,16})/,
+                    message: 'This text has to be an email',
+                }
+            })}/>
+            {errors.email?.message && <p className={styles.error}>{errors.email?.message}</p>}
+
+            <label>{t("password")}</label>
+            <input {...register("password", {
+                required: 'Password is required',
+                pattern: {
+                    value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                    message: 'Your password not strong enough'
+                }
+            })}/>
+            {errors.password?.message && <p className={styles.error}>{errors.password?.message}</p>}
+
+            <button type="button" onClick={() => reset()}>
+                Reset
+            </button>
+            <input type="submit" value="Submit"/>
+        </form>
+    </>
+}
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -3,11 +3,16 @@ import Main from "./pages/Main/Main";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
 import Header from "./layouts/Header/Header";
 import Footer from "./layouts/Footer/Footer";
+import LoginPage from "./pages/LoginPage/LoginPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Main/>,
+        errorElement: <ErrorPage/>
+    }, {
+        path: "/login",
+        element: <LoginPage/>,
         errorElement: <ErrorPage/>
     }
 ])
