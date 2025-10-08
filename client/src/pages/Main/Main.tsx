7277be7589787b758842211e9ee5389fdbe2c95e
lalkazaurus <indiraptor63983@gmail.com>
2025-10-08 20:33:45 +0300

    Main page improvements

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -6,7 +6,17 @@ export default function Main() {
     const { t } = useTranslation()
 
     return <div className="container">
-        <p>{t("hello_world")}</p>
-        <Link to={"/ewefw"}>Crush me</Link>
+        <div className={styles.main}>
+            <div className={styles.info}>
+                <h1>Сьогодні — знання. Завтра — можливості.</h1>
+                <div className={styles.linkBlock}>
+                    <span>Не відкладайте свій успіх на завтра. Навчання — це інвестиція, яка окупиться першою. Зробіть перший крок до нової кар'єри прямо зараз.</span>
+                    <Link to={"/subjects"}>Розпочати навчання</Link>
+                </div>
+            </div>
+            <div>
+                <img src="/main-humans.png"/>
+            </div>
+        </div>
     </div>
 }
